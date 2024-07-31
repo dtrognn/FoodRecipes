a12f0cf3fc5bded95fa1a76d89e3a23cf862f65a
dtrognn <dtrognn@gmail.com>
2024-07-31 11:15:20 +0700

    create StraightLine

---
+++ FoodRecipes/DevPods/FRCommon/Components/StraightLine/StraightLine.swift
@@ -0,0 +1,17 @@
+//
+//  StraightLine.swift
+//  FRCommon
+//
+//  Created by dtrognn on 31/7/24.
+//
+
+import SwiftUI
+
+public struct StraightLine: View {
+    public init() {}
+
+    public var body: some View {
+        Divider().frame(height: 1)
+            .foregroundColor(Color.gray)
+    }
+}
