59c0364675ce02792c194900fa3f09b57050884a
dtrognn <dtrognn@gmail.com>
2024-07-31 11:31:47 +0700

    create AppDelegate

---
+++ FoodRecipes/FoodRecipes/Manager/AppConfigurationManager.swift
@@ -0,0 +1,19 @@
+//
+//  AppConfigurationManager.swift
+//  FoodRecipes
+//
+//  Created by dtrognn on 31/7/24.
+//
+
+import Foundation
+import FRCommon
+
+final class AppConfigurationManager {
+    static let shared = AppConfigurationManager()
+
+    private init() {}
+
+    func loadModuleConfig() {
+        
+    }
+}
