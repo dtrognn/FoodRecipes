db5600813e81ee9c080054f04d7b5b44fb271267
dtrognn <dtrognn@gmail.com>
2024-07-31 11:31:57 +0700

    create ScreenContainer

---
+++ FoodRecipes/DevPods/FRCommon/Components/ScreenContainer/ScreenContainerView.swift
@@ -0,0 +1,47 @@
+//
+//  ScreenContainerView.swift
+//  FRCommon
+//
+//  Created by dtrognn on 31/7/24.
+//
+
+import SwiftUI
+
+public struct ScreenContainerView<Content: View>: View {
+    @EnvironmentObject private var router: Router
+    @ObservedObject public var screenConfiguration: ScreenConfiguration
+    public var content: () -> Content
+
+    public init(_ screenConfiguration: ScreenConfiguration,
+                content: @escaping () -> Content)
+    {
+        self.screenConfiguration = screenConfiguration
+        self.content = content
+    }
+
+    public var body: some View {
+        bodyView
+    }
+
+    var bodyView: some View {
+        return ZStack(alignment: .top) {
+            LinearGradient(colors: [AppStyle.theme.backgroundColor, .white], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack(spacing: AppStyle.layout.zero) {
+                if screenConfiguration.showNaviBar {
+                    NaviBarView(screenConfiguration: screenConfiguration)
+                }
+                content()
+                    .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
+                    .navigationBarHidden(true)
+                    .toolbar(screenConfiguration.hidesBottomBarWhenPushed ? .hidden : .visible, for: .tabBar)
+                    .onDisappear {
+                        if screenConfiguration.hidesBottomBarWhenPushed == true {
+                            screenConfiguration.hidesBottomBarWhenPushed = false
+                        }
+                    }
+            }
+        }.ignoresSafeArea(edges: .bottom)
+    }
+}
