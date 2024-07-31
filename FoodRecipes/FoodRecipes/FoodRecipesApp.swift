4125169d86b6895559fe817f07bfd53f8c64d961
dtrognn <dtrognn@gmail.com>
2024-07-31 10:12:40 +0700

    init project

---
+++ FoodRecipes/FoodRecipes/FoodRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipesApp.swift
+//  FoodRecipes
+//
+//  Created by dtrognn on 31/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
