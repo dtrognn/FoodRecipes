35ce17e855db98591b8b2a04ec79830e5ec76d15
dtrognn <dtrognn@gmail.com>
2024-08-01 08:53:37 +0700

    HomeView: add text categories

--- FoodRecipes/FoodRecipes/Features/Home/HomeView.swift
+++ FoodRecipes/FoodRecipes/Features/Home/HomeView.swift
@@ -18,16 +18,75 @@ struct HomeView: View {
 
     var body: some View {
         ScreenContainerView(screenConfiguration) {
-            VStack {
-                Spacer()
-                Button {
-                    router.push(to: HomeTabDestination.foodsWithCategory)
-                } label: {
-                    Text("Home View")
+            VStack(spacing: AppStyle.layout.standardSpace) {
+                headerView
+
+                FRScrollView {
+                    VStack {
+                        categoriesView
+                    }.padding(.all, AppStyle.layout.standardSpace)
                 }
-                Spacer()
             }
         }
+    }
+}
+
+// MARK: - Header
+
+private extension HomeView {
+    var headerView: some View {
+        return HStack(spacing: AppStyle.layout.zero) {
+            helloUserText
+            Spacer()
+            userImageView
+        }.padding(.horizontal, AppStyle.layout.standardSpace)
+    }
+
+    var helloUserText: some View {
+        return Text(String(format: language("Home_A_02"), "dtrognn"))
+            .font(AppStyle.font.medium18)
+            .foregroundStyle(AppStyle.theme.textNormalColor)
+            .lineLimit(1)
+    }
+
+    var userImageView: some View {
+        return Image(systemName: "person.crop.circle")
+            .resizable()
+            .frame(width: AppStyle.layout.largeSpace, height: AppStyle.layout.largeSpace)
+    }
+}
+
+// MARK: - Categories
+
+private extension HomeView {
+    var categoriesView: some View {
+        return VStack(alignment: .leading, spacing: AppStyle.layout.standardSpace) {
+            categoriesText
+            categoryListView
+        }
+    }
+
+    var categoriesText: some View {
+        return Text(language("Home_A_01"))
+            .font(AppStyle.font.semibold24)
+            .foregroundStyle(AppStyle.theme.textNormalColor)
+    }
+
+    var columns: [GridItem] {
+        return [
+            .init(.flexible(), spacing: AppStyle.layout.standardSpace),
+            .init(.flexible(), spacing: AppStyle.layout.standardSpace),
+        ]
+    }
+
+    var categoryListView: some View {
+        return LazyVGrid(columns: columns, spacing: AppStyle.layout.standardSpace) {
+            ForEach(vm.categories) { category in
+                CategoryItemView(category) { _ in
+                    router.push(to: HomeTabDestination.foodsWithCategory)
+                }
+            }
+        }.padding(.bottom, AppStyle.layout.standardButtonHeight * 2)
     }
 }
 
